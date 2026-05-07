add8787fd44c348e2c937f4f47b93b38a6971cd3
sgautiersociete-web <sgautier.societe@gmail.com>
2026-05-07 10:00:30 +0200

    fix: recréation deconnexion.php

--- public/deconnexion.php
+++ public/deconnexion.php
@@ -1,8 +1,4 @@
 <?php
-define('ROOT_PATH', dirname(__DIR__));
-define('APP_PATH',  ROOT_PATH . '/app');
-require_once APP_PATH . '/config/Session';
-Session::start();
 Session::destroy();
 header('Location: /');
-die();
+exit;
